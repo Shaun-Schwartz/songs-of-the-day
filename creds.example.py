ef150dc7a66fc661d6d6bf99ec5202c3b307ccd3
Shaun Schwartz <me@shaunschwartz.com>
2018-03-17 15:01:00 -0700

    Initialize repo

---
+++ creds.example.py
@@ -0,0 +1,9 @@
+# EMAIL CREDS (used to send email)
+EMAIL_USER = 'user@email.com'
+EMAIL_PWD = 'password'
+# REDDIT API
+CLIENT_ID = 'client_id'
+CLIENT_SECRET = 'CLIENT_SECRET'
+# REDDIT USER CREDS
+REDDIT_USER = 'reddit_user'
+REDDIT_PWD = 'reddit_pwd'
---
+++ reddit_creds.py
@@ -0,0 +1,6 @@
+#API
+CLIENT_ID = 'wD9aesYdkR1EjQ'
+CLIENT_SECRET = 'zoW9GibIEIUn92eFijqRzTBqHS8'
+# USER
+REDDIT_USER = 'mr_november'
+REDDIT_PWD = '05s5y7wWxUZMrV2o'
