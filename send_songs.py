a1fc040591550c8b6d39006f566367904a867a67
Shaun Schwartz <me@shaunschwartz.com>
2018-03-18 12:12:26 -0700

    Be more specific with python version and add a bit of error handling to get_songs() method

--- send_songs.py
+++ send_songs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 import requests
 import requests.auth
 import json
@@ -7,7 +7,7 @@ from send_email import send_email
 from recipients import RECIPIENTS
 
 USER_AGENT = "SongsOfTheDay/0.1 by %s" %(REDDIT_USER)
-NUM_OF_SONGS = 5 # Maximum is 25 
+NUM_OF_SONGS = 5 # Maximum is 25
 
 def get_token():
     client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
@@ -22,13 +22,16 @@ def get_songs():
     headers = {"Authorization": f'bearer {token}', "User-Agent": USER_AGENT}
     response = requests.get("https://oauth.reddit.com/r/listentothis/top/?t=day/.json", headers=headers).json()
     for i in range(NUM_OF_SONGS):
-        title = response["data"]["children"][i]["data"]["secure_media"]["oembed"]["title"]
-        thumbnail = response["data"]["children"][i]["data"]["secure_media"]["oembed"]["thumbnail_url"]
-        media_url = response["data"]["children"][i]["data"]["url"]
-        link = "https://www.reddit.com" + response["data"]["children"][i]["data"]["permalink"]
-        msgText += f"{i+1}: {title} \n{media_url} \n{link} \n \n \n "
+        try:
+            title = response["data"]["children"][i]["data"]["secure_media"]["oembed"]["title"]
+            thumbnail = response["data"]["children"][i]["data"]["secure_media"]["oembed"]["thumbnail_url"]
+            media_url = response["data"]["children"][i]["data"]["url"]
+            link = "https://www.reddit.com" + response["data"]["children"][i]["data"]["permalink"]
+            msgText += f"{i+1}: {title} \n{media_url} \n{link} \n \n \n "
+        except:
+            pass
     return msgText
 
 msg = get_songs()
 
-send_email(EMAIL_USER, EMAIL_PWD, RECIPIENTS, "SongsOfTheDay", msg)
+# send_email(EMAIL_USER, EMAIL_PWD, RECIPIENTS, "SongsOfTheDay", msg)
